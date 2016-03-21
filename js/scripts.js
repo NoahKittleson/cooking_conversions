ac59a420fbd29703a271a806da21bf78d88353d7
Noah Kittleson and Nick Lyman <noah@>
2016-03-21 11:11:54 -0700

    Added more conversions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
-var askForGallons = function() {
+var gallonsToLiters = function() {
 	var gallons = prompt("How many gallons?");
-  alert("That is " + parseInt(gallons) / 0.26417 + " Liters.");
+  alert("That is " + parseInt(gallons) / 0.26417 + " Liter(s).");
+};
+var ouncesToGrams = function() {
+  var ounces = prompt("How many ounces?");
+  alert("That is " + parseInt(ounces) / 0.035274 + " gram(s).");
+};
+var cupsToTeaspoons = function () {
+	var cups = prompt("How many cups?");
+  alert("That is " + cups * 48 + " teaspoon(s).");
+  //alert(calc(parseInt(cups));
+};
+var tablespoonsToQuarts = function() {
+	var tablespoons = prompt("How many tablespoons?");
+  alert("That is " + tablespoons / 64 + " quart(s).");
 };
 
-askForGallons();
+ouncesToGrams();
